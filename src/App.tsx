339be555f69bf0dc4ad621c8fd84879c70e83b2a
Granit <1>
2023-04-05 23:55:01 +0400

    add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,22 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import { ToastContainer } from 'react-toastify'
 import { Footer } from './components/Footer/Footer'
 import { Header } from './components/Header/Header'
 import { Main } from './components/Main/Main'
 
 function App() {
+
   return (
-    <BrowserRouter>
         <div className='appStyles'>
             <Header />
-
             <Routes>
-                <Route path='/' Component={Main}/>
-                {/* <Route path='/login' Component={SignForms}/> */}
-                {/* <Route path='/signup' Component={SignForms}/> */}
-                {/* <Route path='/profile' Component={ProfileForm}/> */}
-                {/* <Route path='/product/:id' Component={ProductDetailPage}/> */}
-                {/* <Route path='/cart' Component={Cart}/> */}
-                {/* <Route path='/favourites' Component={Favourites}/> */}
-                {/* <Route path='/add_product' Component={AddProduct}/> */}
+                <Route path='/' element={<Main />}/> 
+                <Route path='*' element={<Main />} />
             </Routes>
-            
             <Footer />
-
             <ToastContainer />
         </div>
-    </BrowserRouter>
-  )
+    )
 }
 
 export default App
